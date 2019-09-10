af9e8ebf856ed415c0dd2b6d248bae5405a97070
desktopgame <gnndr36@gmail.com>
2019-09-10 11:16:12 +0900

    add comment

--- Planet/gel/asset/IAsset.hpp
+++ Planet/gel/asset/IAsset.hpp
@@ -5,6 +5,20 @@
 #include "../Thread.hpp"
 
 namespace gel {
+/**
+ * IAsset is interface every resource in gel library.
+ * implement resource load/unload system.
+ * asset load system in gel library is load by two step
+ *
+ * - step1
+ * load at another thread.
+ * invoke load() pass Thread::OnBackground into parameter `thread`
+ * in this, onUpdate, onDraw is called on parallel at OpenGL Thread
+ * - step2
+ * load at OpenGL thread.
+ * invoke load() pass Thread::OnGL into parameter `thread`
+ * in this, onUpdate, onDraw is not be called
+ */
 class IAsset {
        public:
         IAsset() = default;
