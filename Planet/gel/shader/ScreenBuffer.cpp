098c3d27a0e53e40f65c0dc7fb8a33645ab92a5f
desktopgame <gnndr36@gmail.com>
2019-09-17 11:07:37 +0900

    fix ScreenBuffer

--- Planet/gel/shader/ScreenBuffer.cpp
+++ Planet/gel/shader/ScreenBuffer.cpp
@@ -73,10 +73,10 @@ void ScreenBuffer::unbind() { frameBuffer.unbind(); }
 void ScreenBuffer::render() {
         glDisable(GL_DEPTH_TEST);
         //glClearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+		//glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
         shader->use();
         glBindTexture(GL_TEXTURE_2D, texture);
         vao.bind();
