8f5f797fe0d9dd65b4ea03fec363458381db102d
desktopgame <gnndr36@gmail.com>
2019-09-18 23:30:27 +0900

    fix

--- Planet/gel/util/enum.hpp
+++ Planet/gel/util/enum.hpp
@@ -12,7 +12,7 @@ enumTy& operator&= (enumTy& a, enumTy b); \
 enumTy& operator^= (enumTy& a, enumTy b);
 
 #define DEF_ENUM_OPERATOR_CPP(enumTy, valTy) \
-enumTy operator~ (enumTy a) { return (T)~(valTy)a; } \
+enumTy operator~ (enumTy a) { return (enumTy)~(valTy)a; } \
 enumTy operator| (enumTy a, enumTy b) { return (enumTy)((valTy)a | (valTy)b); } \
 enumTy operator& (enumTy a, enumTy b) { return (enumTy)((valTy)a & (valTy)b); } \
 enumTy operator^ (enumTy a, enumTy b) { return (enumTy)((valTy)a ^ (valTy)b); } \
