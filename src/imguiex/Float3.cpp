053f2c91d39e37783670f43720f30b0ceaa0f050
desktopgame <koya0306@outlook.jp>
2019-12-20 16:07:36 +0900

    fix

--- src/imguiex/Float3.cpp
+++ src/imguiex/Float3.cpp
@@ -6,6 +6,14 @@ Float3::Float3(const std::string & label, float step) : label(label), value(), s
 }
 
 void Float3::draw() {
-	ImGui::DragFloat3(label.c_str(), &value.x, step, min, max);
+	float ptr[3] = {
+		value.x,
+		value.y,
+		value.x
+	};
+	ImGui::DragFloat3(label.c_str(), ptr, step, min, max);
+	value.x = ptr[0];
+	value.y = ptr[1];
+	value.z = ptr[2];
 }
 }
