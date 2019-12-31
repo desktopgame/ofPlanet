c3f43de0555b0a6945a19803e99ea6854c305976
desktopgame <koya0306@outlook.jp>
2019-12-31 15:33:44 +0900

    update script

--- commit_data.rb
+++ commit_data.rb
@@ -7,4 +7,13 @@ def publish_dir(src, dst)
     FileUtils.cp_r(src, dst)
 end
 
+def publish_file(src, dst)
+    if Dir.exists?(dst)
+        FileUtils.rm(dst)
+    end
+    FileUtils.cp(src, dst)
+end
+
 publish_dir('./appdata/data', './bin/data')
+publish_file('./appdata/blocks.json', './bin/blocks.json')
+publish_file('./appdata/textures.json', './bin/textures.json')
