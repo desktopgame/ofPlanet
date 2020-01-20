1c091db4c36da16b7fa9298d947a417cb9a4c82a
desktopgame <koya0306@outlook.jp>
2020-01-20 10:45:37 +0900

    add comment

--- src/common/WASDMove.hpp
+++ src/common/WASDMove.hpp
@@ -3,17 +3,43 @@
 #define COMMON_WASDMOVE_HPP
 #include <ofVec3f.h>
 namespace planet {
-
+/**
+ * WASDMove は、一人称視点のゲームにおけるキーボード操作で一般的なWASDをサポートします。
+ */
 class WASDMove {
        public:
         explicit WASDMove();
 
+		/**
+		 * 有効化します。
+		 */
         void enable();
+
+		/**
+		 * 無効化します。
+		 */
         void disable();
+
+		/**
+		 * キー入力を検出します。
+		 */
         void update();
 
+		/**
+		 * スプリント中なら 2 を、それ以外なら 1 を返します。
+		 * @return
+		 */
         float getScale() const;
+
+		/**
+		 * 現在の移動方向を返します。
+		 * @return
+		 */
         glm::ivec2 getAxis() const;
+
+		/**
+		 * スプリント中なら true を返します。
+		 */
         bool isSprint() const;
 
        private:
