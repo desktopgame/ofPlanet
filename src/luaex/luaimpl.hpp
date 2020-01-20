c2a04d6b0c896d43c9a6962893f4fb9271b4c4e7
desktopgame <koya0306@outlook.jp>
2020-01-20 12:26:25 +0900

    fix include order

--- src/luaex/luaimpl.hpp
+++ src/luaex/luaimpl.hpp
@@ -1,8 +1,10 @@
 #pragma once
 #ifndef LUAEX_LUAIMPL_HPP
 #define LUAEX_LUAIMPL_HPP
-#include <lauxlib.h>
-#include <lualib.h>
 
 #include <lua.hpp>
+//--この順番でインクルードすること
+#include <lauxlib.h>
+//--
+#include <lualib.h>
 #endif
